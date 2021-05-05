f507c9b3eee6c6b029febdaea1d56edced9d3bb5
Alejandro Almonacid <alejandroalmonaciderazo21@gmail.com>
2021-05-05 15:42:17 -0400

    Se agrega diseño
    Se agrega diseño y se deja script por separado

---
+++ script.js
@@ -0,0 +1,16 @@
+const boton = document.getElementsByTagName('button')[0];
+    //Manipulamos DOM para ejecutar programa
+    boton.addEventListener('click', () =>{
+        //Creamos un arreglo donde salga cada nombre
+        const atracadores = [
+            {nombre: "Rio"}, 
+            {nombre: "Tokio"}, 
+            {nombre: "Moscú"}, 
+            {nombre: "Lisboa"}, 
+            {nombre: "Berlín"}]
+        const pregunta = document.getElementsByTagName("input")[0].value;
+        //Buscamos dentro del arreglo y lo comparamos con la respuesta del prompt
+        //Se usa operador ternario, tambien se puede hacer con un switch o if else
+        const respuesta = (atracadores.find(a=>a.nombre === pregunta)) ? "Si es miembro del equipo, vamos por el": "No existe"
+        alert(respuesta);
+    })
