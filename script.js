789e282a49f495a02434d28ee569f7cfd7eced15
Alejandro Almonacid <alejandroalmonaciderazo21@gmail.com>
2021-05-05 18:19:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const boton = document.getElementsByTagName('button')[0];
-    //Manipulamos DOM para ejecutar programa
-    boton.addEventListener('click', () =>{
-        //Creamos un arreglo donde salga cada nombre
-        const atracadores = [
-            {nombre: "Rio"}, 
-            {nombre: "Tokio"}, 
-            {nombre: "Moscú"}, 
-            {nombre: "Lisboa"}, 
-            {nombre: "Berlín"}]
-        const pregunta = document.getElementsByTagName("input")[0].value;
-        //Buscamos dentro del arreglo y lo comparamos con la respuesta del prompt
-        //Se usa operador ternario, tambien se puede hacer con un switch o if else
-        const respuesta = (atracadores.find(a=>a.nombre === pregunta)) ? "Si es miembro del equipo, vamos por el": "No existe"
-        
-        alert(respuesta);
-    })
+//Manipulamos DOM para ejecutar programa
+boton.addEventListener('click', () => {
+    const pregunta = document.getElementsByTagName("input")[0].value;
+    //Creamos un arreglo donde salga cada nombre
+    const atracadores = [
+        { nombre: "Rio" },
+        { nombre: "Tokio" },
+        { nombre: "Moscú" },
+        { nombre: "Lisboa" },
+        { nombre: "Berlín" }]
+    //usamos operador ternario, tambien se puede hacer con un switch o if else
+    const respuesta = (atracadores.find(a => a.nombre === pregunta)) ? "Si es miembro del equipo, vamos por el" : "No existe"
+    alert(respuesta);
+})
